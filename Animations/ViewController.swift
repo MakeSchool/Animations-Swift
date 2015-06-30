e711cd3f0ff5247bc2d8b82fdb0aa0ef1c77131f
Ben-G <Benjamin.Encz@gmail.com>
2015-06-30 15:49:59 -0700

    dragging complete

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     @IBOutlet weak var largerHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var arrowLabel: UILabel!
@@ -20,10 +19,72 @@ class ViewController: UIViewController {
     var viewExpaned = false
     var blurView: UIView!
     var popupView: UIView!
+    var snapBehavior : UISnapBehavior!
+    var animator: UIDynamicAnimator!
+    var attachmentBehavior: UIAttachmentBehavior!
+    var dynamicItemBehavior: UIDynamicItemBehavior!
+    var gestureRecognizer: UIPanGestureRecognizer!
+    var gravityBehavior: UIGravityBehavior!
+    var draggingPopup = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        animator = UIDynamicAnimator(referenceView: view)
+    }
     
     @IBAction func closeTransactionViewTapped(sender: AnyObject) {
         blurView.removeFromSuperview()
         popupView.removeFromSuperview()
+        self.view.removeGestureRecognizer(gestureRecognizer)
+    }
+    
+    
+    func handlePan(recognizer: UIPanGestureRecognizer) {
+        if (recognizer.state == .Began) {
+            if (!CGRectContainsPoint(popupView.frame,recognizer.locationInView(self.view))) {
+                return
+            }
+            
+            draggingPopup = true
+            
+            attachmentBehavior = UIAttachmentBehavior(
+                item: popupView,
+                attachedToAnchor: recognizer.locationInView(self.view)
+            )
+            
+            attachmentBehavior.damping = 2
+            attachmentBehavior.frequency = 10
+            attachmentBehavior.length = 0
+            attachmentBehavior.anchorPoint = recognizer.locationInView(self.view)
+            animator.addBehavior(attachmentBehavior)
+            dynamicItemBehavior = UIDynamicItemBehavior(items: [popupView])
+            animator.addBehavior(dynamicItemBehavior)
+        } else if (recognizer.state == .Changed) {
+            if (draggingPopup) {
+                attachmentBehavior.anchorPoint = recognizer.locationInView(self.view)
+            }
+        } else if (recognizer.state == .Ended || recognizer.state == .Cancelled) {
+            if !(draggingPopup) {
+                return
+            }
+            
+            animator.removeBehavior(attachmentBehavior)
+            attachmentBehavior = nil
+            
+            let p1 = popupView.center
+            let p2 = self.view.center
+            let distance = hypotf(Float(p1.x) - Float(p2.x), Float(p1.y) - Float(p2.y))
+            
+            if (distance > 50) {
+                gravityBehavior = UIGravityBehavior(items: [popupView])
+                animator.addBehavior(gravityBehavior)
+                
+                animator.removeBehavior(snapBehavior)
+                snapBehavior = nil
+                self.view.removeGestureRecognizer(gestureRecognizer)
+            }
+        }
     }
     
     @IBAction func animateButtonTapped(sender: AnyObject) {
@@ -43,12 +104,17 @@ class ViewController: UIViewController {
     }
  
     @IBAction func transactionButtonTapped(sender: AnyObject) {
+        gestureRecognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        self.view.addGestureRecognizer(gestureRecognizer)
+        
         popupView = NSBundle.mainBundle().loadNibNamed("TransactionView", owner: self, options: nil)[0] as! UIView
         popupView.center = CGPointMake(-300, -300)
         self.view.addSubview(popupView)
         
         let blurEffect = UIBlurEffect(style: .Light)
-        blurView = UIVisualEffectView(effect: blurEffect)
+        blurView = UIView()
+        blurView.backgroundColor = UIColor.darkGrayColor()
+        blurView.alpha = 0.0
         blurView.setTranslatesAutoresizingMaskIntoConstraints(false)
         
         self.view.insertSubview(self.blurView, atIndex: self.view.subviews.count-1)
@@ -58,18 +124,20 @@ class ViewController: UIViewController {
             toItem: self.blurView.superview, attribute: .Width,
             multiplier: 1, constant: 0))
         
-        self.view.layoutIfNeeded()
-        
         self.view.addConstraint(NSLayoutConstraint(item: self.blurView,
             attribute: .Height, relatedBy: .Equal,
             toItem: self.blurView.superview, attribute: .Height,
             multiplier: 1, constant: 0))
+
+        self.view.layoutIfNeeded()
         
         UIView.animateWithDuration(0.75, delay: 0.0, usingSpringWithDamping: 0.65, initialSpringVelocity: 1.0, options: .CurveEaseIn, animations: { () -> Void in
             self.popupView.center = self.view.center
+            self.blurView.alpha = 0.85
             self.view.layoutIfNeeded()
-        }, completion: nil)
-        
-  
+            }, completion: { (Bool) -> Void in
+                self.snapBehavior = UISnapBehavior(item: self.popupView, snapToPoint: self.view.center)
+                self.animator.addBehavior(self.snapBehavior)
+        })
     }
 }
