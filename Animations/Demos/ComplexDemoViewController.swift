393da285b2616fbea20fdd32e9b97476f9817b22
Ben-G <Benjamin.Encz@gmail.com>
2015-07-01 16:23:37 -0700

    restructure complex animation

--- Animations/Demos/ComplexDemoViewController.swift
+++ Animations/Demos/ComplexDemoViewController.swift
@@ -16,7 +16,7 @@ class ComplexDemoViewController: UIViewController {
     @IBOutlet var buttons: [UIButton]!
     
     // View References
-    var blurView: UIView!
+    var overlayView: UIView!
     var popupView: UIView!
     
     // UIKit Dynamics
@@ -29,8 +29,9 @@ class ComplexDemoViewController: UIViewController {
 
     // View State
     var draggingPopup = false
-    var viewExpaned = false
-
+    var viewExpanded = false
+    
+    // MARK: View Lifecyle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,17 +39,62 @@ class ComplexDemoViewController: UIViewController {
         animator = UIDynamicAnimator(referenceView: view)
     }
     
-    @IBAction func closeTransactionViewTapped(sender: AnyObject) {
-        popupView.removeFromSuperview()
-        self.view.removeGestureRecognizer(gestureRecognizer)
+    // MARK: Drag Handling
+    
+    func startDragging(initalPosition: CGPoint) {
+        draggingPopup = true
         
-        UIView.animateWithDuration(0.35, animations: { () -> Void in
-            self.blurView.alpha = 0.0
-        }, completion: { (Bool) -> Void in
-            self.blurView.removeFromSuperview()
-        })
+        attachmentBehavior = UIAttachmentBehavior(
+            item: popupView,
+            attachedToAnchor: initalPosition
+        )
+        
+        // clean up gravity behavior if it exists
+        if gravityBehavior != nil {
+            animator.removeBehavior(gravityBehavior)
+            gravityBehavior = nil
+        }
+        
+        attachmentBehavior.damping = 2
+        attachmentBehavior.frequency = 10
+        attachmentBehavior.length = 0
+        attachmentBehavior.anchorPoint = initalPosition
+        
+        dynamicItemBehavior = UIDynamicItemBehavior(items: [popupView])
+        
+        dynamicItemBehavior.action = {() -> Void in
+            if (!CGRectIntersectsRect(self.popupView.frame, self.view.frame)) {
+                self.closeTransactionViewTapped(self)
+                self.draggingPopup = false
+                self.animator.removeAllBehaviors()
+            }
+        }
+        
+        animator.addBehavior(attachmentBehavior)
+        animator.addBehavior(dynamicItemBehavior)
     }
     
+    func endDragging() {
+        animator.removeBehavior(attachmentBehavior)
+        attachmentBehavior = nil
+        
+        let p1 = popupView.center
+        let p2 = self.view.center
+        let distance = hypotf(Float(p1.x) - Float(p2.x), Float(p1.y) - Float(p2.y))
+        
+        if (distance > 50) {
+            draggingPopup = false
+            self.view.removeGestureRecognizer(gestureRecognizer)
+            
+            gravityBehavior = UIGravityBehavior(items: [popupView])
+            animator.addBehavior(gravityBehavior)
+            
+            animator.removeBehavior(snapBehavior)
+            snapBehavior = nil
+        }
+    }
+    
+    // MARK: Touch Handling
     
     func handlePan(recognizer: UIPanGestureRecognizer) {
         if (recognizer.state == .Began) {
@@ -56,28 +102,7 @@ class ComplexDemoViewController: UIViewController {
                 return
             }
             
-            draggingPopup = true
-            
-            attachmentBehavior = UIAttachmentBehavior(
-                item: popupView,
-                attachedToAnchor: recognizer.locationInView(self.view)
-            )
-            
-            attachmentBehavior.damping = 2
-            attachmentBehavior.frequency = 10
-            attachmentBehavior.length = 0
-            attachmentBehavior.anchorPoint = recognizer.locationInView(self.view)
-            animator.addBehavior(attachmentBehavior)
-            dynamicItemBehavior = UIDynamicItemBehavior(items: [popupView])
-            animator.addBehavior(dynamicItemBehavior)
-            
-            dynamicItemBehavior.action = {() -> Void in
-                if (!CGRectIntersectsRect(self.popupView.frame, self.view.frame)) {
-                    self.closeTransactionViewTapped(self)
-                    self.draggingPopup = false
-                    self.animator.removeAllBehaviors()
-                }
-            }
+            startDragging(recognizer.locationInView(self.view))
         } else if (recognizer.state == .Changed) {
             if (draggingPopup) {
                 attachmentBehavior.anchorPoint = recognizer.locationInView(self.view)
@@ -87,75 +112,91 @@ class ComplexDemoViewController: UIViewController {
                 return
             }
             
-            animator.removeBehavior(attachmentBehavior)
-            attachmentBehavior = nil
-            
-            let p1 = popupView.center
-            let p2 = self.view.center
-            let distance = hypotf(Float(p1.x) - Float(p2.x), Float(p1.y) - Float(p2.y))
-            
-            if (distance > 50) {
-                draggingPopup = false
-                gravityBehavior = UIGravityBehavior(items: [popupView])
-                animator.addBehavior(gravityBehavior)
-                
-                animator.removeBehavior(snapBehavior)
-                snapBehavior = nil
-                self.view.removeGestureRecognizer(gestureRecognizer)
-            }
+            endDragging()
         }
     }
     
-    @IBAction func animateButtonTapped(sender: AnyObject) {
-        viewExpaned = !viewExpaned
-        heightConstraint.active = !viewExpaned
+    
+    // MARK: View State
+    
+    func toggleExpandMenu() {
+        viewExpanded = !viewExpanded
+        heightConstraint.active = !viewExpanded
         
         UIView.animateWithDuration(0.25, delay: 0.0, options: .CurveEaseInOut, animations: { () -> Void in
-            
             for button in self.buttons {
-                button.alpha = self.viewExpaned ? 1.0 : 0.0
+                button.alpha = self.viewExpanded ? 1.0 : 0.0
             }
             
-            self.arrowButton.transform = self.viewExpaned ? CGAffineTransformMakeRotation(CGFloat(M_PI_2)) : CGAffineTransformMakeRotation(0)
+            self.arrowButton.transform = self.viewExpanded ? CGAffineTransformMakeRotation(CGFloat(M_PI_2)) : CGAffineTransformMakeRotation(0)
             self.view.layoutIfNeeded()
-        }, completion: nil)
+            }, completion: nil)
     }
- 
-    @IBAction func transactionButtonTapped(sender: AnyObject) {
+    
+    func presentTransaction() {
+        // Add gesture recognizer to detect touches
         gestureRecognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
         self.view.addGestureRecognizer(gestureRecognizer)
         
+        // Present TransactionView (loaded from separate interface file)
         popupView = NSBundle.mainBundle().loadNibNamed("TransactionView", owner: self, options: nil)[0] as! UIView
         popupView.center = CGPointMake(-300, -300)
         self.view.addSubview(popupView)
         
-        let blurEffect = UIBlurEffect(style: .Light)
-        blurView = UIView()
-        blurView.backgroundColor = UIColor.darkGrayColor()
-        blurView.alpha = 0.0
-        blurView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        // Load and add overlay view
+        overlayView = UIView()
+        overlayView.backgroundColor = UIColor.darkGrayColor()
+        overlayView.alpha = 0.0
+        overlayView.setTranslatesAutoresizingMaskIntoConstraints(false)
         
-        self.view.insertSubview(self.blurView, atIndex: self.view.subviews.count-1)
+        self.view.insertSubview(overlayView, atIndex: view.subviews.count-1)
         
-        self.view.addConstraint(NSLayoutConstraint(item: self.blurView,
+        self.view.addConstraint(NSLayoutConstraint(item: overlayView,
             attribute: .Width, relatedBy: .Equal,
-            toItem: self.blurView.superview, attribute: .Width,
+            toItem: overlayView.superview, attribute: .Width,
             multiplier: 1, constant: 0))
         
-        self.view.addConstraint(NSLayoutConstraint(item: self.blurView,
+        self.view.addConstraint(NSLayoutConstraint(item: overlayView,
             attribute: .Height, relatedBy: .Equal,
-            toItem: self.blurView.superview, attribute: .Height,
+            toItem: overlayView.superview, attribute: .Height,
             multiplier: 1, constant: 0))
-
-        self.view.layoutIfNeeded()
         
+        // Force Layout pass, so that adding of the background view is not animated
+        view.layoutIfNeeded()
+        
+        // Animate popover onto screen, blend in overlay
         UIView.animateWithDuration(0.75, delay: 0.0, usingSpringWithDamping: 0.65, initialSpringVelocity: 1.0, options: .CurveEaseIn, animations: { () -> Void in
             self.popupView.center = self.view.center
-            self.blurView.alpha = 0.85
+            self.overlayView.alpha = 0.85
             self.view.layoutIfNeeded()
             }, completion: { (Bool) -> Void in
                 self.snapBehavior = UISnapBehavior(item: self.popupView, snapToPoint: self.view.center)
                 self.animator.addBehavior(self.snapBehavior)
         })
+    }
+    
+    func hideTransaction() {
+        popupView.removeFromSuperview()
+        view.removeGestureRecognizer(gestureRecognizer)
+        
+        UIView.animateWithDuration(0.35, animations: { () -> Void in
+            self.overlayView.alpha = 0.0
+            }, completion: { (Bool) -> Void in
+                self.overlayView.removeFromSuperview()
+        })
+    }
+    
+    // MARK: Button Callbacks
+    
+    @IBAction func animateButtonTapped(sender: AnyObject) {
+        toggleExpandMenu()
+    }
+ 
+    @IBAction func transactionButtonTapped(sender: AnyObject) {
+        presentTransaction()
+    }
+    
+    @IBAction func closeTransactionViewTapped(sender: AnyObject) {
+        hideTransaction()
     }
 }
